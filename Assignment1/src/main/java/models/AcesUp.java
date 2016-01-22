f66c8bbada319ab64a84c8498de3f4f762145091
siebertc <siebertc@oregonstate.edu>
2016-01-21 18:13:47 -0800

    Removed todo Class

--- Assignment1/src/main/java/models/AcesUp.java
+++ Assignment1/src/main/java/models/AcesUp.java
@@ -1,31 +1,7 @@
 package models;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-
 /**
- * Created by michaelhilton on 1/14/16.
+ * Created by Charles Siebert on 1/21/2016.
  */
-public class ToDoList implements Serializable {
-    public java.util.List<String> todoItems;
-    public String item;
-    public String action;
-
-
-    public ToDoList(){
-        todoItems = new ArrayList<String>();
-    }
-
-    public void addTodoItem(String item) {
-        todoItems.add(item);
-    }
-
-    public int numberOfItems() {
-        return todoItems.size();
-    }
-
-    public String lastItemAdded() {
-        return todoItems.get(todoItems.size()-1);
-    }
-
+public class AcesUp {
 }
